ee93dbd1a3da8678809acafd76b7e858e0807460
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2021-11-19 16:56:41 +0200

    add font and navbar

--- index.php
+++ index.php
@@ -9,7 +9,18 @@
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-hello momen
+    <div class="bar1 navbar fixed-top ">
+        <button class="navbar-toggler">
+            <span class="navbar-toggler-icon"> </span>
+        </button>
 
+<!--        border-->
+        <div class="container">
+           <div class="col">
+               <img src="img/pic1.png" alt="">GOODental
+           </div>
+        </div>
+    </div>
+<span></span>
 </body>
 </html>
