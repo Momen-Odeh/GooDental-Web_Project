a259b409b31fc518068ac486d552c50ee9fb7f69
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2021-11-19 10:29:53 +0200

    basic info

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>GOODENTAL</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 </head>
 <body>
 
+<div>
 
+</div>
 </body>
 </html>
