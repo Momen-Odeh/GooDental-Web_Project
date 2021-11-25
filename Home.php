3cea6ceca3864e72bb0fa3fb3325181786517afa
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2021-11-25 21:44:34 +0200

    update3

--- Home.php
+++ Home.php
@@ -31,10 +31,101 @@
 </nav>
 <div class="spaceofnav"></div>
 <div class="imgbg">
-    <div class="maintk">
-        <h1 class="titlede ">We Build <br>Your Smile</h1>
+    <h1 class="titlede ">We Build <br>Your Smile</h1>
+    <script>
+        window.addEventListener('scroll',()=>{
+            let scrl=window.scrollY;
+            console.log(scrl);
+            if(scrl >= 0 && scrl<=160)
+            {
+                let scrl1=scrl-50;
+                scrl1=scrl1/100;
+                document.getElementById("cards").style.opacity=scrl1;
+                document.getElementById("cards").style.transform = 'translate'+'('+0+'px,'+'-'+scrl+ 'px)';
+            }
 
+        });
+    </script>
+    <div id="cards" class="maintk"  >
+
+<!---->
+        <div class="cardss">
+        <div class="card border-info mb-3" style="max-width: 18rem; background-color: #80ffcc ">
+            <div class="card-header">Header</div>
+            <div class="card-body" style="background: #80ffcc ">
+                <h5 class="card-title">Info card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+<!--        -->
+<!---->
+        <div class="cardss" >
+            <div class="card border-info mb-3" style="max-width: 18rem; background-color: #80ffcc ">
+                <div class="card-header">Header</div>
+                <div class="card-body" style="background: #80ffcc ">
+                    <h5 class="card-title">Info card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+<!--        -->
+        <!---->
+        <div class="cardss" ">
+            <div class="card border-info mb-3" style="max-width: 18rem; background-color: #80ffcc ">
+                <div class="card-header">Header</div>
+                <div class="card-body" style="background: #80ffcc ">
+                    <h5 class="card-title">Info card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <!--        -->
+        <!---->
+        <div class="cardss"">
+            <div class="card border-info mb-3" style="max-width: 18rem; background-color: #80ffcc ">
+                <div class="card-header">Header</div>
+                <div class="card-body" style="background: #80ffcc ">
+                    <h5 class="card-title">Info card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <!--        -->
     </div>
 </div>
+jvjsk
+<pre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</pre>
 </body>
 </html>
