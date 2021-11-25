6219d43dd528a922b14baf512a50e51f2b0d0f37
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2021-11-25 14:32:18 +0200

    Nav-bar for project

---
+++ Home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GOODENTAL</title>
+    <link rel="stylesheet" href="styleHOME.css">
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav>
+<!--  for munebar  -->
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+<!--    -->
+    <label class="logo">GOODental</label>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">location</a></li>
+        <li><a href="#">timing</a></li>
+        <li><a href="#">who developed</a></li>
+    </ul>
+</nav>
+<div class="spaceofnav"></div>
+<section class="bgimg"></section>
+<pre>
+
+
+
+
+
+
+
+
+</pre>
+</body>
+</html>
