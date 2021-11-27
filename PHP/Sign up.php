802ecbbab081f843a4adf29c44f2e7c8982003ba
moheedeab <moheedeab16@gmail.com>
2021-11-27 19:43:18 +0200

    27/11/2021

---
+++ PHP/Sign up.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LOGIN</title>
+    <link rel="stylesheet" href="../CSS/signup.css.css">
+</head>
+<body>
+
+
+</body>
+</html>
