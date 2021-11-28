befea8918f6a45060e90e795be33dcec2db2f87a
moheedeab <moheedeab16@gmail.com>
2021-11-29 01:55:40 +0200

    28/11/2021

--- PHP/Sign up.php
+++ PHP/Sign up.php
@@ -7,31 +7,31 @@
 </head>
 <body>
 
-<img class="cover" src="../img/boy-brushing1.jpg" id="image">
-
 
 <div class="container">
     <div class="header">
         <h1 >Sign Up</h1>
-    </div>
+    </div  >
     <div class="main">
         <form>
+
             <input  type="text" placeholder="Username" >
-            <input  type="Date" placeholder="Username" >
-            <span>
-            <input name="gender"  type="radio">Male
-            <input name="gender"  type="radio">Female
-            </span>
-            <input type="text" placeholder="Mobile number" >
+            </br>
+            <input type="Date">
+            <br>
+
+            <input name="gender"  type="radio" data-text="Male"><label></label>
+            <input name="gender"  type="radio"><label>Female</label>
+            <br>
+            <input type="number" placeholder="Mobile number" >
             <input type="password" placeholder="Password" >
             <button class="b"> Sign Up</button>
         </form>
     </div>
 
+
 </div>
 <script>
-
-
 
 </body>
 </html>
