001ac7a06e9be9fb059703e5c9af4609d4afe7a3
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2021-11-29 16:58:07 +0200

    Momen Update 16 29/11/2021

--- PHP/Profile.php
+++ PHP/Profile.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>LOGIN</title>
+    <title>Profile</title>
     <link rel="stylesheet" href="../CSS/styleHOME.css">
     <link rel="stylesheet" href="../CSS/c1.css">
     <script src="../SCRIPT/script.js"></script>
@@ -27,10 +27,86 @@
     <ul>
         <li><a href="../PHP/Home.php">Home</a></li>
         <li><a href="../PHP/Service.php">Services</a></li>
-        <li><a href="#">Location</a></li>
+        <li><a href="../PHP/Contact_Location.php">Contact Us</a></li>
         <li><a href="../PHP/Login.php">Sign in</a></li>
     </ul>
 </nav>
-
+<div class="spaceofnav"></div>
+<div class="main">
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 mt-1">
+            <div class="card text-center sidebar">
+                <div class="card-body">
+                    <img src="../img/d8.jpg" class="rounded-circle" width="150" alt="">
+                    <div class="mt-3">
+                        <h3>Momen Odeh</h3>
+                        <a href="../PHP/Home.php">Home</a>
+                        <a href="../PHP/Service.php">Services</a>
+                        <a href="../PHP/Contact_Location.php">Contact Us</a>
+                        <a href="../PHP/Home.php">Sign out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 mt-1">
+            <div class="card mb-3 content">
+                <h1 class="m-3 pt-3">About</h1>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h5>Full Name</h5>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                        Momen Odeh
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h5>Email</h5>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            Momen.odeh74@gmail.com
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h5>Phone</h5>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            0597627566
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h5>Address</h5>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            Palstine-Nablus
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+            <div class="card mb-3 content">
+                <h1 class="m-3">Recent Project</h1>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h5>Project Name</h5>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            Project Description
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 </body>
 </html>
